15f6f3dc48eff298cd929b0bac245a6910ba8c3d
Zhuklevich Gleb <zhuklevich.gleb@gmail.com>
2025-06-30 17:23:10 +0300

    feat: add cleaner

--- model.py
+++ model.py
@@ -5,13 +5,16 @@ from typing import Union
 
 
 class Model:
-    def __init__(self, path2model: str | Path):
+    def __init__(self, path2model: str | Path, clear=True):
         self.path2model = (
             Path(path2model) if isinstance(path2model, str) else path2model
         )
         self.path2output = self.path2model / "output"
         self.path2fit = self.path2model / "fitlist.txt"
+        self.clear = clear
         self._model_anal()
+
+
 
     def _read_and_parse_file(self, file_path: Path) -> pd.DataFrame:
         with open(file_path, "r") as f:
@@ -36,6 +39,10 @@ class Model:
     @property
     def model_data(self) -> pd.DataFrame | list[Union[np.ndarray, list]]:
         full_data = self._read_and_parse_file(self.path2output)
+        if self.clear:
+            full_data["chi_squared"] = pd.to_numeric(full_data["chi_squared"])
+            full_data = full_data.loc[(full_data["flag_error"] == "00000000") & (full_data["chi_squared"] < 10)]
+            
 
         if self.spectra_num > 1:
             print("output file is compicated")
@@ -56,7 +63,6 @@ class Model:
                 print("Output file is broken")
                 breakpoint()
 
-            # models_data = np.array((self.spectra_teff, self.spectra_logg, self.spectra_feh))
             models_data = []
             for params in range(len(data_cube)):
                 models_data.append([self.spectra_teff[params], self.spectra_logg[params], self.spectra_feh[params]])
@@ -71,7 +77,7 @@ class Model:
     def fitlist_data(self) -> pd.DataFrame:
         return self._read_and_parse_file(self.path2fit)
 
-    # cursed..
+    # cursed.. may be keywords are good idea
     def _model_anal(self) -> None:
         fitlist = self.fitlist_data
         self.spectra_names = fitlist.values[:, 0]
@@ -88,4 +94,3 @@ if __name__ == "__main__":
     data_path = "/home/epsilon/TSFitPy/output_files/05113_teff/"
     m = Model(data_path)
     d = m.model_data
-    print(d)
